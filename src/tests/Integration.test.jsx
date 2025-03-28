1944bf24620b0f14565a53e41232cf232490dd94
myaOlaye <136443656+myaOlaye@users.noreply.github.com>
2025-03-28 14:33:07 +0000

    test: add integration test for loading states and correct rendering of the restaurant list

--- src/tests/Integration.test.jsx
+++ src/tests/Integration.test.jsx
@@ -30,67 +30,102 @@ describe("App", () => {
       expect(screen.getByText("Steakhouse Grill")).toBeInTheDocument();
     });
   });
-});
 
-it("shows a list of under 10 restaurants when a valid postcode is inputted that has less than 10 restaurants", async () => {
-  axios.get.mockResolvedValue({
-    data: {
-      restaurants: mockRestaurants.slice(0, 5),
-    },
+  it("shows a list of under 10 restaurants when a valid postcode is inputted that has less than 10 restaurants", async () => {
+    axios.get.mockResolvedValue({
+      data: {
+        restaurants: mockRestaurants.slice(0, 5),
+      },
+    });
+
+    render(<App></App>);
+
+    const input = screen.getByRole("textbox");
+    fireEvent.change(input, { target: { value: "KT23 4HL" } });
+
+    const submitButton = screen.getByRole("button");
+    fireEvent.click(submitButton);
+
+    await waitFor(() => {
+      const restaurants = screen.getAllByRole("article");
+      expect(restaurants.length).toBe(5);
+      expect(screen.getByText("The Gourmet Spot")).toBeInTheDocument();
+      expect(screen.getByText("Vegan Bites")).toBeInTheDocument();
+    });
   });
 
-  render(<App></App>);
+  it("shows server error message when api request fails", async () => {
+    axios.get.mockRejectedValue(new Error("API error"));
 
-  const input = screen.getByRole("textbox");
-  fireEvent.change(input, { target: { value: "KT23 4HL" } });
+    render(<App></App>);
 
-  const submitButton = screen.getByRole("button");
-  fireEvent.click(submitButton);
+    const input = screen.getByRole("textbox");
+    fireEvent.change(input, { target: { value: "KT23 4HL" } });
 
-  await waitFor(() => {
-    const restaurants = screen.getAllByRole("article");
-    expect(restaurants.length).toBe(5);
-    expect(screen.getByText("The Gourmet Spot")).toBeInTheDocument();
-    expect(screen.getByText("Vegan Bites")).toBeInTheDocument();
+    const submitButton = screen.getByRole("button");
+    fireEvent.click(submitButton);
+
+    await waitFor(() => {
+      expect(
+        screen.getByText("Sorry, something went wrong. Please try again later.")
+      ).toBeInTheDocument();
+    });
   });
-});
 
-it("shows server error message when api request fails", async () => {
-  axios.get.mockRejectedValue(new Error("API error"));
+  it("shows not operating in your area message when no restaurants are found for a valid postcode", async () => {
+    axios.get.mockResolvedValue({
+      data: {
+        restaurants: [],
+      },
+    }); // Assuming Just Eat's API returns an empty array for a valid postcode with no resturants
 
-  render(<App></App>);
+    render(<App></App>);
 
-  const input = screen.getByRole("textbox");
-  fireEvent.change(input, { target: { value: "KT23 4HL" } });
+    const input = screen.getByRole("textbox");
+    fireEvent.change(input, { target: { value: "KT23 4HL" } });
 
-  const submitButton = screen.getByRole("button");
-  fireEvent.click(submitButton);
+    const submitButton = screen.getByRole("button");
+    fireEvent.click(submitButton);
 
-  await waitFor(() => {
-    expect(
-      screen.getByText("Sorry, something went wrong. Please try again later.")
-    ).toBeInTheDocument();
+    await waitFor(() => {
+      expect(
+        screen.getByText("Sorry, we are not operating in your area yet.")
+      ).toBeInTheDocument();
+    });
   });
-});
+  it("correctly shows loading message and does not show any previously rendered restaurants whilst loading new restaurants (i.e., whilst requests to the API are pending)", async () => {
+    axios.get.mockResolvedValue({
+      data: {
+        restaurants: mockRestaurants,
+      },
+    });
 
-it("shows not operating in your area message when no restaurants are found for a valid postcode", async () => {
-  axios.get.mockResolvedValue({
-    data: {
-      restaurants: [],
-    },
-  }); // Assuming Just Eat's API returns an empty array for a valid postcode with no resturants
+    render(<App></App>);
 
-  render(<App></App>);
+    const input = screen.getByRole("textbox");
+    const submitButton = screen.getByRole("button");
 
-  const input = screen.getByRole("textbox");
-  fireEvent.change(input, { target: { value: "KT23 4HL" } });
+    fireEvent.change(input, { target: { value: "KT23 4HL" } });
+    fireEvent.click(submitButton);
 
-  const submitButton = screen.getByRole("button");
-  fireEvent.click(submitButton);
+    expect(screen.queryByText("Loading...")).toBeInTheDocument();
 
-  await waitFor(() => {
-    expect(
-      screen.getByText("Sorry, we are not operating in your area yet.")
-    ).toBeInTheDocument();
+    await waitFor(() => {
+      expect(screen.getByText("The Gourmet Spot")).toBeInTheDocument();
+      expect(screen.getByText("Steakhouse Grill")).toBeInTheDocument();
+    });
+
+    fireEvent.change(input, { target: { value: "E16 3NN" } });
+    fireEvent.click(submitButton);
+
+    expect(screen.queryByText("The Gourmet Spot")).not.toBeInTheDocument();
+    expect(screen.queryByText("Sushi Haven")).not.toBeInTheDocument();
+    expect(screen.queryByText("Loading...")).toBeInTheDocument();
+
+    await waitFor(() => {
+      expect(screen.getByText("The Gourmet Spot")).toBeInTheDocument();
+      expect(screen.getByText("Sushi Haven")).toBeInTheDocument();
+      expect(screen.queryByText("Loading...")).not.toBeInTheDocument();
+    });
   });
 });
