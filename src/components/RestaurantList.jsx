2faa381f4f4fb7227ee15202a0e5efbaf4d352a0
myaOlaye <136443656+myaOlaye@users.noreply.github.com>
2025-03-28 22:50:10 +0000

    feat: add sorting by restaur ant rating to RestaurantList component

--- src/components/RestaurantList.jsx
+++ src/components/RestaurantList.jsx
@@ -1,13 +1,30 @@
-import React from "react";
+import { React, useState } from "react";
 import RestaurantCard from "./RestaurantCard";
 import styles from "./RestaurantList.module.css";
 
 const RestaurantList = ({ restaurants, loading }) => {
-  // add a filter here to ordr restaurants by rating
+  const [orderByRating, setOrderByRating] = useState(false);
+
+  const sortedRestaurants = orderByRating
+    ? [...restaurants].sort((a, b) => b.rating.starRating - a.rating.starRating)
+    : restaurants;
 
   return loading ? null : (
     <section className={styles.restaurantListSection}>
-      {restaurants.map((restaurant) => {
+      {restaurants.length > 0 && (
+        <div>
+          <input
+            type="checkbox"
+            name="orderByRating"
+            id="orderByRating"
+            checked={orderByRating}
+            onChange={() => setOrderByRating(!orderByRating)}
+          />
+          <label htmlFor="orderByRating">Order by rating</label>
+        </div>
+      )}
+
+      {sortedRestaurants.map((restaurant) => {
         return (
           <RestaurantCard
             key={restaurant.id}
